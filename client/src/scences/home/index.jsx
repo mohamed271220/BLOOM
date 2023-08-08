2c4d188910b61af3d3b08ec408119f8d3783cdc6
mohamed271220 <mohamedaprahym@gmail.com>
2023-08-08 11:36:43 +0300

    almost done

--- client/src/scences/home/index.jsx
+++ client/src/scences/home/index.jsx
@@ -1,11 +1,36 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 import Navbar from "scences/navbar";
+import UserWidget from "scences/widgets/UserWidget";
+import MyPostWidget from "scences/widgets/MyPostWidget";
 
 const Home = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <Box>
       <Navbar />
+      <Box
+        width={"100%"}
+        p="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap={"0.5rem"}
+        justifyContent={"space-between"}
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+        </Box>
+
+        {isNonMobileScreens && <Box flexBasis={"26%"}></Box>}
+      </Box>
     </Box>
   );
 };
